ef728d51b356de08c461cc06fc379cc5cbf07b6a
Ryan  Hanna <ryan.hanna15@gmail.com>
2017-11-21 11:05:14 -0600

    updated

--- reacttraversy/projectmanager/src/Components/ProjectItem.js
+++ reacttraversy/projectmanager/src/Components/ProjectItem.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 
 class ProjectItem extends Component {
+  deleteProject(id){
+    this.props.onDelete(id);
+
+  }
+
   render() {
     return (
       <li className="Projects">
-      <strong>{this.props.project.title}</strong>: - {this.props.project.category}
+      <strong>{this.props.project.title}</strong>: - {this.props.project.category} <a href="#" onClick={this.deleteProject.bind(this, this.props.project.id)}>x</a>
     </li>
     );
   }
+}
+
+ProjectItem.propTypes = {
+project: React.PropTypes.object,
+onDelete: React.PropTypes.func
+
 }
 
 export default ProjectItem;
--- reacttraversy/projectmanager/src/Components/Projects.js
+++ reacttraversy/projectmanager/src/Components/Projects.js
@@ -1,24 +1,36 @@
 import React, { Component } from 'react';
 import ProjectItem from './ProjectItem';
+import PropTypes from 'prop-types';
 
 class Projects extends Component {
+  deleteProject(id){
+    this.props.onDelete(id);
+
+  }
   render() {
     let projectItems;
     if(this.props.projects){
       projectItems = this.props.projects.map(project => {
         // console.log(project);
         return(
-          <ProjectItem key={project.title} project={project} />
+          <ProjectItem onDelete={this.deleteProject.bind(this)} key={project.title} project={project} />
 
         );
       });
     }
     return (
       <div className="Projects">
+<h3>Latest Projects </h3>
       {projectItems}
       </div>
     );
   }
+}
+
+Projects.propTypes = {
+projects: React.PropTypes.string,
+onDelete: React.PropTypes.func
+
 }
 
 export default Projects;
